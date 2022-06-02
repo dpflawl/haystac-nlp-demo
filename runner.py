acae79893bb4484b78085a0108dd85d7ce039a74
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-06-02 11:47:28 +0900

    Update runner.py

--- runner.py
+++ runner.py
@@ -13,6 +13,14 @@ input = st.text_area('입력', '''
      안녕하세요. 반갑습니다.
      ''')
 
+if 'count' not in st.session_state or st.session_state.count == 6:
+ st.session_state.count = 0 
+ st.session_state.chat_history_ids = None
+ st.session_state.old_response = ''
+else:
+ st.session_state.count += 1
+
+
 tokenizer = AutoTokenizer.from_pretrained(
   'kakaobrain/kogpt', revision='KoGPT6B-ryan1.5b-float16',
   bos_token='[BOS]', eos_token='[EOS]', unk_token='[UNK]', pad_token='[PAD]', mask_token='[MASK]')
@@ -21,25 +29,16 @@ model = AutoModelForCausalLM.from_pretrained(
   pad_token_id=tokenizer.eos_token_id,
   torch_dtype=torch.float16, low_cpu_mem_usage=True)
 
-if 'count' not in st.session_state or st.session_state.count == 6:
- st.session_state.count = 0 
- st.session_state.chat_history_ids = None
- st.session_state.old_response = ''
-else:
- st.session_state.count += 1
-new_user_input_ids = tokenizer.encode(input + tokenizer.eos_token, return_tensors='pt')
+with torch.no_grad():
+    new_user_input_ids = tokenizer.encode(input + tokenizer.eos_token, return_tensors='pt')
+    bot_input_ids = torch.cat([st.session_state.chat_history_ids, new_user_input_ids], dim=-1) if st.session_state.count > 1 else new_user_input_ids
+    st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id)
 
-bot_input_ids = torch.cat([st.session_state.chat_history_ids, new_user_input_ids], dim=-1) if st.session_state.count > 1 else new_user_input_ids
-
-st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id)
-
-response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
+    response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
 
 if st.session_state.old_response == response:
   bot_input_ids = new_user_input_ids
-
   st.session_state.chat_history_ids = model.generate(bot_input_ids, max_length=5000, pad_token_id=tokenizer.eos_token_id) 
-
   response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
 
 st.subheader('챗봇 답변')
