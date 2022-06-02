4feac26e79d1136e74d2ae07d2bec5a75b611fba
Yerim Ji <89132581+dpflawl@users.noreply.github.com>
2022-06-02 17:20:32 +0900

    Update runner.py

--- runner.py
+++ runner.py
@@ -11,7 +11,7 @@ import torch
 st.title("감정 모델 기반의 챗봇 서비스👾")
     
 def get_text():
-    input_text = st.text_input("You: ","안녕하세요, 반가워요.", key="input")
+    input_text = st.text_input("You: ","안녕하세요. 반가워요🖐", key="input")
     return input_text 
 
 user_input = get_text()
@@ -22,6 +22,7 @@ if 'generated' not in st.session_state:
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
+    st.session_state.chat_history_ids = None
 
 if user_input:
     tokenizer = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
@@ -31,14 +32,15 @@ if user_input:
 
     with torch.no_grad():
         new_user_input_ids = tokenizer.encode(user_input + tokenizer.eos_token, return_tensors='pt')
-        st.session_state.chat_history_ids = model.generate(new_user_input_ids,
+        bot_input_ids = torch.cat([st.session_state.chat_history_ids, new_user_input_ids], dim=-1)
+        st.session_state.chat_history_ids = model.generate(bot_input_ids,
                                                             max_length=128,
                                                             repetition_penalty=2.0,
                                                             pad_token_id=tokenizer.pad_token_id,
                                                             eos_token_id=tokenizer.eos_token_id,
                                                             bos_token_id=tokenizer.bos_token_id,
                                                             use_cache=True)
-        response = tokenizer.decode(st.session_state.chat_history_ids[:, new_user_input_ids.shape[-1]:][0], skip_special_tokens=True)       
+        response = tokenizer.decode(st.session_state.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)       
         st.session_state.past.append(user_input)
         st.session_state.generated.append(response)
 
